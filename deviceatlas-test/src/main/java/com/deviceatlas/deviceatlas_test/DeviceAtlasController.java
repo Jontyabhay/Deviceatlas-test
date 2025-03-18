a4df623f0314088c9abca66f52370e06956d894e
Akshay Mishra <akshaymishra94@gmail.com>
2025-03-18 13:47:10 +0000

    tablet devices only sorted by osversion

--- deviceatlas-test/src/main/java/com/deviceatlas/deviceatlas_test/DeviceAtlasController.java
+++ deviceatlas-test/src/main/java/com/deviceatlas/deviceatlas_test/DeviceAtlasController.java
@@ -1,7 +1,9 @@
 package com.deviceatlas.deviceatlas_test;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,6 +12,9 @@ public class DeviceAtlasController {
 
     private final DeviceAtlasService deviceAtlasService;
 
+    @Autowired
+    private DeviceAtlasRepo deviceAtlasRepo;
+
     public DeviceAtlasController(DeviceAtlasService deviceAtlasService) {
         this.deviceAtlasService = deviceAtlasService;
     }
@@ -17,5 +22,13 @@ public class DeviceAtlasController {
     @GetMapping("/")
     public List<String> detectDeviceProperties() {
         return deviceAtlasService.processandstoreUserAgents();
+    }
+
+    @GetMapping("/tablets")
+    public List<DeviceAtlasProperties> getTablets() {
+        return deviceAtlasRepo.findAll().stream()
+                .filter(device -> "tablet".equalsIgnoreCase(device.getPrimaryHardwareType()))
+                .sorted((d1, d2) -> d1.getOsVersion().compareTo(d2.getOsVersion()))
+                .collect(Collectors.toList());
     }
 }
